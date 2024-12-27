98bbfaabff8d90f67ad1c2f4358075791aaba428
quic-khrahul <131336334+quic-khrahul@users.noreply.github.com>
2024-12-27 17:48:15 +0530

    Update script to download the config JSON file
    Signed-off-by: quic-khrahul <131336334+quic-khrahul@users.noreply.github.com>

--- download_artifacts.sh
+++ download_artifacts.sh
@@ -40,6 +40,7 @@ download_labels() {
     local url=$1
     local output_dir=$2
     curl -L -O "$url" && unzip -o "$(basename "$url")" && \
+    cp labels/yolonas.labels labels/yolov8.labels
     cp labels/* "$output_dir"
 }
 
@@ -49,6 +50,13 @@ download_file() {
     local output_dir=$2
     curl -L -O "$url"
     cp "$(basename "$url")" "$output_dir"
+}
+
+# Function to download configs
+download_config() {
+    local url=$1
+    local output_dir=$2
+    curl -L -o "$output_dir" "$url"
 }
 
 # Main script execution
@@ -81,16 +89,26 @@ main() {
     download_labels "https://github.com/quic/sample-apps-for-qualcomm-linux/releases/download/${version}/labels.zip" "/opt"
 
     # Download model and label files
-    download_file "https://huggingface.co/qualcomm/Inception-v3-Quantized/resolve/main/Inception-v3-Quantized.tflite" "/opt/"
-    download_file "https://huggingface.co/qualcomm/DeepLabV3-Plus-MobileNet-Quantized/resolve/main/DeepLabV3-Plus-MobileNet-Quantized.tflite" "/opt/"
+    download_file "https://huggingface.co/qualcomm/Inception-v3-Quantized/resolve/main/Inception-v3-Quantized.tflite" "/opt/inception_v3_quantized.tflite"
+    download_file "https://huggingface.co/qualcomm/DeepLabV3-Plus-MobileNet-Quantized/resolve/main/DeepLabV3-Plus-MobileNet-Quantized.tflite" "/opt/deeplabv3_plus_mobilenet_quantized.tflite"
     download_file "https://huggingface.co/qualcomm/Midas-V2-Quantized/resolve/main/Midas-V2-Quantized.tflite" "/opt/"
-    download_file "https://huggingface.co/qualcomm/HRNetPoseQuantized/resolve/main/HRNetPoseQuantized.tflite" "/opt/"
+    download_file "https://huggingface.co/qualcomm/HRNetPoseQuantized/resolve/main/HRNetPoseQuantized.tflite" "/opt/hrnet_pose_quantized.tflite"
     download_file "https://huggingface.co/qualcomm/Yolo-NAS-Quantized/resolve/main/Yolo-NAS-Quantized.tflite" "/opt/"
     download_file "https://huggingface.co/qualcomm/YOLOv8-Detection-Quantized/resolve/main/YOLOv8-Detection-Quantized.tflite" "/opt/"
+    download_file "https://huggingface.co/qualcomm/YOLOv8-Detection-Quantized/resolve/main/YOLOv8-Detection-Quantized.tflite" "/opt/yolov8_det_quantized.tflite"
     download_file "https://qaihub-public-assets.s3.us-west-2.amazonaws.com/qai-hub-models/models/midas/midasv2_linux_assets/midasv2.dlc" "/opt/"
     download_file "https://qaihub-public-assets.s3.us-west-2.amazonaws.com/qai-hub-models/models/midas/midasv2_linux_assets/monodepth.labels" "/opt/"
+    download_file "https://huggingface.co/qualcomm/QuickSRNetSmall-Quantized/resolve/main/QuickSRNetSmall-Quantized.tflite" "/opt/quicksrnetsmall_quantized.tflite"
 
-    echo "Model and Label files download Successful to /opt directory"
+    # Download config files
+    download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-classification/config_classification.json?inline=false" "/opt/config_classification.json"
+    download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-daisychain-detection-classification/config_daisychain_detection_classification.json?inline=false" "/opt/config_daisychain_detection_classification.json"
+    download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-monodepth/config_monodepth.json?inline=false" "/opt/config_monodepth.json"
+    download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-object-detection/config_detection.json?inline=false" "/opt/config_detection.json"
+    download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-pose-detection/config_pose.json?inline=false" "/opt/config_pose.json"
+    download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-segmentation/config_segmentation.json?inline=false" "/opt/config_segmentation.json"
+
+    echo "Model,Label and Config files download Successful to /opt directory"
     exit 0
 }
 
