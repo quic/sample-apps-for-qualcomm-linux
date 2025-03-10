9e795113209adb4adc45c54052b99f723d4f986f
quic-khrahul <131336334+quic-khrahul@users.noreply.github.com>
2025-03-10 13:24:26 +0530

    Update script to download GA1.4 models
    Command:
    ./download_artifacts.sh -v GA1.4-rel -c QCS6490
    
    Signed-off-by: quic-khrahul <131336334+quic-khrahul@users.noreply.github.com>

--- download_artifacts.sh
+++ download_artifacts.sh
@@ -12,11 +12,11 @@ show_help() {
     echo "Options:"
     echo "  -v, --version    Select the release version to download (e.g., GA1.3-rel)"
     echo "  -c, --chipset    Select the chipset directory to copy files from (e.g., QCS6490, QCS9075)"
-    echo "  -o, --outpath    Select the output directory to copy files to (e.g., /tmp . This is an optional parameter. Defaults to /opt if not provided  )"
+    echo "  -o, --outpath    Select the output directory to copy files to (e.g., /tmp). This is an optional parameter. Defaults to /opt if not provided."
     echo "  -h, --help       Help menu"
     echo
     echo "Example:"
-    echo "  ./download_artifacts.sh -v GA1.3-rel -c QCS6490"
+    echo "  ./download_artifacts.sh -v GA1.4-rel -c QCS6490"
 }
 
 # Function to check internet connectivity
@@ -59,7 +59,6 @@ check_env_vars () {
 download_models() {
     local url=$1
     local output_dir=$2
-    local chipset=$3
     curl -L -O "$url" && unzip -o "$(basename "$url")" && \
     mv "$(basename "$url" .zip)"/* "$output_dir"
     #Cross Check : should it be cp or mv cp "$(basename "$url" .zip)"/* "$output_dir"
@@ -102,13 +101,12 @@ main() {
 
     check_env_vars
     if [ ! -z "$env_sdk_version" ]; then
-      version="${env_sdk_version}"
+        version="${env_sdk_version}"
     fi
-    if [ ! -z "$env_chipset"  ]; then
-      chipset="${env_chipset}"
+    if [ ! -z "$env_chipset" ]; then
+        chipset="${env_chipset}"
     fi
-    
-    
+
     # Parse command-line arguments
     while [[ "$#" -gt 0 ]]; do
         case $1 in
@@ -121,45 +119,79 @@ main() {
         shift
     done
 
+    if [ "$version" == "GA1.3-rel" ]; then
+        outputmodelpath="$outputpath"
+        outputlabelpath="$outputpath"
+        outputconfigpath="$outputpath"
+    else
+        outputmodelpath="/etc/models/"
+        outputlabelpath="/etc/labels/"
+        outputconfigpath="/etc/configs/"
+    fi
+
     # Check if version and chipset are provided
     if [[ -z "$version" || -z "$chipset" ]]; then
         echo "Error: Both version and chipset must be specified."
         show_help
         exit 1
     fi
-    mkdir -p ${outputpath}
-    if [ -w "${outputpath}" ]; then
-      echo "Models and Labels for ${chipset} Version ${version}  will be downloaded to ${outputpath}"
+
+    mkdir -p "${outputmodelpath}"
+    mkdir -p "${outputlabelpath}"
+    mkdir -p "${outputconfigpath}"
+
+    if [ -w "${outputmodelpath}" ] && [ -w "${outputlabelpath}" ] && [ -w "${outputconfigpath}" ]; then
+        if [ "$version" == "GA1.3-rel" ]; then
+            echo "Models, Labels, and Configs for ${chipset} Version ${version} will be downloaded to ${outputpath}"
+        else
+            echo "Models and Labels for ${chipset} Version ${version} will be downloaded to /etc/models and /etc/labels/"
+        fi
     else
-      echo "${outputpath} does not have write permissions."
-      echo "Please provide a writable directory using -o option"
-      exit 1
+        if [ "$version" == "GA1.3-rel" ]; then
+            echo "${outputpath} does not have write permissions."
+        else
+            echo "/etc/ does not have write permissions."
+            echo "Please provide a writable directory using -o option"
+            exit 1
+        fi
     fi
 
-    check_internet
+    #check_internet
 
     # Download and unzip the specified version
-    download_models "https://github.com/quic/sample-apps-for-qualcomm-linux/releases/download/${version}/v2.29_${chipset}.zip" ${outputpath} "$chipset"
-    download_labels "https://github.com/quic/sample-apps-for-qualcomm-linux/releases/download/${version}/labels.zip" ${outputpath} 
+    if [ "$version" == "GA1.3-rel" ]; then
+      download_models "https://github.com/quic/sample-apps-for-qualcomm-linux/releases/download/${version}/v2.29_${chipset}.zip" ${outputmodelpath}
+      download_labels "https://github.com/quic/sample-apps-for-qualcomm-linux/releases/download/${version}/labels.zip" ${outputlabelpath} 
+    else
+      download_models "https://github.com/quic/sample-apps-for-qualcomm-linux/releases/download/GA1.4-rel/v2.31_${chipset}.zip" ${outputmodelpath}
+      download_labels "https://github.com/quic/sample-apps-for-qualcomm-linux/releases/download/GA1.4-rel/labels.zip" ${outputlabelpath} 
+	fi
 
     # Download model and label files
-    download_file "https://huggingface.co/qualcomm/Inception-v3-Quantized/resolve/main/Inception-v3-Quantized.tflite" "${outputpath}/inception_v3_quantized.tflite"
-    download_file "https://huggingface.co/qualcomm/DeepLabV3-Plus-MobileNet-Quantized/resolve/main/DeepLabV3-Plus-MobileNet-Quantized.tflite" "${outputpath}/deeplabv3_plus_mobilenet_quantized.tflite"
-    download_file "https://huggingface.co/qualcomm/Midas-V2-Quantized/resolve/main/Midas-V2-Quantized.tflite" "${outputpath}"
-    download_file "https://huggingface.co/qualcomm/HRNetPoseQuantized/resolve/main/HRNetPoseQuantized.tflite" "${outputpath}/hrnet_pose_quantized.tflite"
-    download_file "https://qaihub-public-assets.s3.us-west-2.amazonaws.com/qai-hub-models/models/midas/midasv2_linux_assets/midasv2.dlc" "${outputpath}/"
-    download_file "https://qaihub-public-assets.s3.us-west-2.amazonaws.com/qai-hub-models/models/midas/midasv2_linux_assets/monodepth.labels" "${outputpath}/"
-    download_file "https://huggingface.co/qualcomm/QuickSRNetSmall-Quantized/resolve/main/QuickSRNetSmall-Quantized.tflite" "${outputpath}/quicksrnetsmall_quantized.tflite"
+    download_file "https://huggingface.co/qualcomm/Inception-v3-Quantized/resolve/main/Inception-v3-Quantized.tflite" "${outputmodelpath}/inception_v3_quantized.tflite"
+    download_file "https://huggingface.co/qualcomm/DeepLabV3-Plus-MobileNet-Quantized/resolve/main/DeepLabV3-Plus-MobileNet-Quantized.tflite" "${outputmodelpath}/deeplabv3_plus_mobilenet_quantized.tflite"
+    download_file "https://huggingface.co/qualcomm/Midas-V2-Quantized/resolve/main/Midas-V2-Quantized.tflite" "${outputmodelpath}"
+    download_file "https://huggingface.co/qualcomm/HRNetPoseQuantized/resolve/main/HRNetPoseQuantized.tflite" "${outputmodelpath}/hrnet_pose_quantized.tflite"
+    download_file "https://qaihub-public-assets.s3.us-west-2.amazonaws.com/qai-hub-models/models/midas/midasv2_linux_assets/midasv2.dlc" "${outputmodelpath}/"
+    download_file "https://qaihub-public-assets.s3.us-west-2.amazonaws.com/qai-hub-models/models/midas/midasv2_linux_assets/monodepth.labels" "${outputlabelpath}/"
+    download_file "https://huggingface.co/qualcomm/QuickSRNetSmall-Quantized/resolve/main/QuickSRNetSmall-Quantized.tflite" "${outputmodelpath}/quicksrnetsmall_quantized.tflite"
 
-    # Download config files
-    download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-classification/config_classification.json?inline=false" "${outputpath}/config_classification.json"
-    download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-daisychain-detection-classification/config_daisychain_detection_classification.json?inline=false" "${outputpath}/config_daisychain_detection_classification.json"
-    download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-monodepth/config_monodepth.json?inline=false" "${outputpath}/config_monodepth.json"
-    download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-object-detection/config_detection.json?inline=false" "${outputpath}/config_detection.json"
-    download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-pose-detection/config_pose.json?inline=false" "${outputpath}/config_pose.json"
-    download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-segmentation/config_segmentation.json?inline=false" "${outputpath}/config_segmentation.json"
+    if [ "$version" == "GA1.3-rel" ]; then
+        # Download config files
+      download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-classification/config_classification.json?inline=false" "${outputconfigpath}/config_classification.json"
+      download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-daisychain-detection-classification/config_daisychain_detection_classification.json?inline=false" "${outputconfigpath}/config_daisychain_detection_classification.json"
+      download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-monodepth/config_monodepth.json?inline=false" "${outputconfigpath}/config_monodepth.json"
+      download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-object-detection/config_detection.json?inline=false" "${outputconfigpath}/config_detection.json"
+      download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-pose-detection/config_pose.json?inline=false" "${outputconfigpath}/config_pose.json"
+      download_config "https://git.codelinaro.org/clo/le/platform/vendor/qcom-opensource/gst-plugins-qti-oss/-/raw/imsdk.lnx.2.0.0.r2-rel/gst-sample-apps/gst-ai-segmentation/config_segmentation.json?inline=false" "${outputconfigpath}/config_segmentation.json"
+    fi
 
-    echo "Model,Label and Config files download Successful to ${outputpath} directory"
+    if [ "$version" == "GA1.3-rel" ]; then
+      echo "Model,Label and Config files download Successful to ${outputpath} directory"
+	else 
+      echo "Model and Label files download Successful to /etc/models/ and /etc/labels/ directory"
+    fi
+
     exit 0
 }
 
