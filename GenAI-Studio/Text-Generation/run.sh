176c2058089178d460bd49881f3a075115167965
quic-khrahul <131336334+quic-khrahul@users.noreply.github.com>
2025-10-01 10:41:22 +0530

    Delete GenAI-Studio directory
    Signed-off-by: quic-khrahul <131336334+quic-khrahul@users.noreply.github.com>

--- GenAI-Studio/Text-Generation/run.sh
+++
@@ -1,7 +0,0 @@
-# ---------------------------------------------------------------------
-# Copyright (c) Qualcomm Innovation Center, Inc. All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause
-# ---------------------------------------------------------------------
-
-#! /bin/bash
-llamachat --genie-config /opt/genie_bundle/genie_config.json --base-dir /opt/genie_bundle/
